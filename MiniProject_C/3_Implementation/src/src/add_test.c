b03930b0614a16525b7b00bb6cc887f34f1eded6
Koushani Das <koushanionline@gmail.com>
2020-10-08 00:19:55 +0530

    Add files via upload

---
+++ MiniProject_C/3_Implementation/src/src/add_test.c
@@ -0,0 +1,19 @@
+
+#include<add_test.h>
+#include<struct.h>
+void add_test()
+{
+    
+    printf("\n");
+    fflush(stdin);
+    printf("Enter total number of tests done = \n");
+    scanf("%d",&y[0].total);
+    fflush(stdin);
+    printf("Enter number of test results positive = \n");
+    scanf("%d",&y[0].pos);
+    fflush(stdin);
+    printf("Enter number of test results negative = \n");
+    scanf("%d",&y[0].neg);
+    fflush(stdin);
+    printf("\n");
+}
