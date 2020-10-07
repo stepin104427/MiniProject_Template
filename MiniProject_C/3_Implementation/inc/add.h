4809a6d3aee604a1c4e5c66e3290a00c9e3668e1
Koushani Das <koushanionline@gmail.com>
2020-10-08 00:03:14 +0530

    Add files via upload

---
+++ MiniProject_C/3_Implementation/inc/add.h
@@ -0,0 +1,10 @@
+#ifndef __VIEW_H__
+#define __VIEW_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+void view()
+ 
+
+#endif  
---
+++ MiniProject_C/3_Implementation/inc/search.h
@@ -0,0 +1,10 @@
+#ifndef __EDIT_H__
+#define __EDIT_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+void edit()
+ 
+
+#endif  
