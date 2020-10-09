bfe5e5ce3f62ddb86c4809a3e402f50453a45e5c
Koushani Das <koushanionline@gmail.com>
2020-10-09 23:02:24 +0530

    Update project_main_new.c

--- MiniProject_C/3_Implementation/project_main_new.c
+++ MiniProject_C/3_Implementation/project_main_new.c
@@ -1,29 +1,136 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
+/*!
+ * @struct      ad
+ * @abstract    Contains all information fields of patient.
+ * @discussion  All properties of patients (name, age, ...)
+ *              are stored in this structure.
+ * @field       name        Name of patient.
+ * @field       status      Status(Positive, recovered, deceased).
+ * @field       bedno       Bed number of the patient.
+ * @field       phone       Phone number of patient
+ * @field       age         Age of patient
+ * @var         x
+ * @abstract    Array variable of type struct ad
+ * @discussion  Array variable that stores information of
+ *              a maximum of 200 patients at any given time.
+*/
 struct ad
 {
     char name[30];
     char status[30];
     int bedno,phone,age;
 } x[200];
+/*!
+ * @struct      tests
+ * @abstract    Contains test reports of a single day.
+ * @discussion  All details of test reports (total, positive, ...)
+ *              are stored in this structure.
+ * @field       total     Total tests done in a single day.
+ * @field       pos       Number of test results positive.
+ * @field       neg       Number of test results negative.
+ * 
+ * @var         y
+ * @abstract    Array variable of type struct tests
+ * @discussion  Array variable that stores information about
+ *              test results on a single day.
+*/
 struct tests
 {
     long total,pos,neg;
 }y[1];
+/*!
+ * @var         bed_count
+ * @abstract    Current beds available
+ * @discussion  Variable that stores the number of beds
+ *              available at any given time.
+ * @var         num
+ * @abstract    Number of records in the database
+ * @discussion  Variable to store number of records present
+ *              in the database.
+*/
 
 int bed_count=200,num;
+/*!
+ * @function    read
+ * @abstract    Read from the patient file and statistics file.
+ * @discussion  This function uses 2 pointers, reads the records of patients
+ *              from the patient file and test reports from 
+ *              statistics file.
+ * @result      Pointers to the patient file and statistics file.
+*/
 void read();
+/*!
+ * @function    add
+ * @abstract    Add information of new patient in the database.
+ * @discussion  This function takes input from the user, accepts
+ *              details of new patient and adds it to the database. 
+ * @result      Record of new patient added to database.
+*/
 void add();
+/*!
+ * @function    view
+ * @abstract    View the entire database.
+ * @discussion  This function shows all the records of patients 
+ *              stored in the database. 
+ * @result      Entire database is shown.
+*/
 void view();
+/*!
+ * @function    search
+ * @abstract    Searches information of patient in the database.
+ * @discussion  This function takes a request from the user, searches
+ *              details of patient requested by the user and 
+ *              shows it from the database. 
+ * @result      Record of patient requested by user shown from database.
+*/
 void search();
+/*!
+ * @function    edit
+ * @abstract    Edit information of patient in the database.
+ * @discussion  This function takes a request from the user, searches
+ *              details of patient requested by the user and 
+ *              edits it in the database. 
+ * @result      Record of patient edited in database.
+*/
 void edit();
+/*!
+ * @function    del
+ * @abstract    Delete information of patient from the database.
+ * @discussion  This function takes input from the user, searches
+ *              details of patient requested by the user and 
+ *              deletes specific information in the database. 
+ * @result      Record of patient deleted in database.
+*/
 void del();
+/*!
+ * @function    write
+ * @abstract    Write in the patient file and statistics file.
+ * @discussion  This function uses 2 pointers, writes the records of patients
+ *              in the patient file and test reports in 
+ *              statistics file.
+ * @result      Information written in the patient file and statistics file.
+*/
 void write();
+/*!
+ * @function    add_test
+ * @abstract    Update test reports in statistics file.
+ * @discussion  This function takes input from the user and
+ *              updates information about the test reports in 
+ *              statistics file daily. 
+ * @result      Test results updated in statistics file.
+*/
 void add_test();
 int main()
 {
     read();
+    /*!
+    * @var         c
+    * @abstract    Choice selected from given options.
+    * @discussion  Variable to store choice selected by user to
+    *              take action from the given list.
+    */
     int c;
     printf("Simple Hospital Management System\n");
     
@@ -93,6 +200,15 @@ int main()
 }
 void add()
 {
+    /*!
+    * @var         n
+    * @abstract    User input to add the number of records to database.
+    * @discussion  Variable to store number of records to be added to
+    *              database given as input by user.
+    * @var         sum
+    * @abstract    Store sum of n and num.
+    * @discussion  Variable to store total number of records present in the database.
+    */
     int n,sum;
     printf("\n\n");
     printf("Already data inputed on the database =%d\n\n",num);//how many inputs
@@ -101,6 +217,15 @@ void add()
     sum=n+num;
     bed_count=bed_count-n;
 
+    /*!
+    * @var         i
+    * @abstract    Loop variable from num to sum.
+    * @discussion  Variable to enter details of patient as input from user
+    *              for n number of times.
+    * @var         j
+    * @abstract    Counter variable.
+    * @discussion  Variable to count number of records entered in database.
+    */
     for(int i=num,j=0; i<sum; i++)
     {
         printf("\n");
@@ -128,6 +253,12 @@ void add()
 
 void view()
 {
+    /*!
+    * @var         i
+    * @abstract    Loop variable from 0 to num.
+    * @discussion  Variable to view details of all patients in the
+    *              database.
+    */
     for(int i=0; i<num; i++)
     {
         printf("\n");
@@ -142,6 +273,16 @@ void view()
 }
 void edit()
 {
+     /*!
+    * @var         q
+    * @abstract    Choice selected from given options.
+    * @discussion  Variable to store choice selected by user to
+    *              edit specific details of a patient.
+    * @var         p
+    * @abstract    Serial number of patient.
+    * @discussion  Stores the serial number of the patient
+    *              whose details are to be edited. 
+    */
     int q,p;
     fflush(stdin);
     printf("What do you want to edit ?\n");
@@ -201,6 +342,20 @@ void edit()
 }
 void search()
 {
+     /*!
+    * @var         s
+    * @abstract    Serial number of patient.
+    * @discussion  Variable to store serial number of patient
+    *              whose details are to be searched.
+    * @var         h
+    * @abstract    Choice selected from given list
+    * @discussion  Variable to store the field by which the patient
+    *              details are to be searched.
+    * @var         u
+    * @abstract    Array variable to accept details of patient
+    * @discussion  Variable to take input from user about the details
+    *              of the patient record to be searched.
+    */
     int s,h;
     char u[100];
     printf("By what do you want to search ?\n");
@@ -226,11 +381,23 @@ void search()
     }
     else if(h==2)
     {
+        /*!
+        * @var         f
+        * @abstract    Temporary variable.
+        * @discussion  Variable to check if details of patient to be
+        *              searched by name are found or not.
+        */
         int f=1;
         fflush(stdin);
         printf("Enter your name=");
         gets(u);
         fflush(stdin);
+         /*!
+        * @var         g
+        * @abstract    Loop variable.
+        * @discussion  Variable to repeat a loop for num
+        *              number of times.
+        */
         for(int g=0; g<num; g++)
         {
             if(strcmp(u,x[g].name)==0)
@@ -252,11 +419,23 @@ void search()
     }
     else if(h==3)
     {
+        /*!
+        * @var         f
+        * @abstract    Temporary variable.
+        * @discussion  Variable to check if details of patient to be
+        *              searched by status are found or not.
+        */
         int f=1;
         fflush(stdin);
         printf("Enter Status = ");
         gets(u);
         fflush(stdin);
+         /*!
+        * @var         g
+        * @abstract    Loop variable.
+        * @discussion  Variable to repeat a loop for num
+        *              number of times.
+        */
         for(int g=0; g<num; g++)
         {
             if(strcmp(u,x[g].status)==0)
@@ -277,9 +456,21 @@ void search()
     }
     else if(h==4)
     {
+        /*!
+        * @var         f
+        * @abstract    Temporary variable.
+        * @discussion  Variable to check if details of patient to be
+        *              searched by bed number are found or not.
+        */
         int f=1;
         printf("Enter Bed number = ");
         scanf("%d",&f);
+         /*!
+        * @var         g
+        * @abstract    Loop variable.
+        * @discussion  Variable to repeat a loop for num
+        *              number of times.
+        */
         for(int g=0; g<num; g++)
         {
             if(f==x[g].bedno)
@@ -300,9 +491,21 @@ void search()
     }
     else if(h==5)
     {
+        /*!
+        * @var         f
+        * @abstract    Temporary variable.
+        * @discussion  Variable to check if details of patient to be
+        *              searched by phone number are found or not.
+        */
         int f=1;
         printf("Enter Phone number = ");
         scanf("%d",&f);
+         /*!
+        * @var         g
+        * @abstract    Loop variable.
+        * @discussion  Variable to repeat a loop for num
+        *              number of times.
+        */
         for(int g=0; g<num; g++)
         {
             if(f==x[g].phone)
@@ -323,9 +526,21 @@ void search()
     }
     else if(h==6)
     {
+        /*!
+        * @var         f
+        * @abstract    Temporary variable.
+        * @discussion  Variable to check if details of patient to be
+        *              searched by age are found or not.
+        */
         int f=1;
         printf("Enter Age = ");
         scanf("%d",&f);
+         /*!
+        * @var         g
+        * @abstract    Loop variable.
+        * @discussion  Variable to repeat a loop for num
+        *              number of times.
+        */
         for(int g=0; g<num; g++)
         {
             if(f==x[g].age)
@@ -349,6 +564,16 @@ void search()
 }
 void del()
 {
+    /*!
+        * @var         f
+        * @abstract    Serial number of patient.
+        * @discussion  Variable to store serial number of patient
+        *              to be deleted given as input bu user.
+        * @var         h
+        * @abstract    Choice selected from given list
+        * @discussion  Variable to store choice of details to deleted
+        *              of a specific patient.
+        */
     int f,h;
     printf("Enter the serial number of the patient that you want to delete=");
     scanf("%d",&f);
@@ -398,6 +623,16 @@ void del()
 }
 void read()
 {
+    /*!
+        * @var         fp
+        * @abstract    File pointer variable.
+        * @discussion  Pointer variable to read information of
+        *              patients from the patient file.
+        * @var         fptr
+        * @abstract    File pointer variable.
+        * @discussion  Pointer variable to read test records of a 
+        *              single day from the statistics file.
+        */
     FILE *fp = fopen("patient.txt","r");
     if(fp == NULL)
     {
@@ -424,6 +659,16 @@ void read()
 }
 void write()
 {
+    /*!
+        * @var         fp
+        * @abstract    File pointer variable.
+        * @discussion  Pointer variable to write information of
+        *              patients into the patient file.
+        * @var         fptr
+        * @abstract    File pointer variable.
+        * @discussion  Pointer variable to write test records of a 
+        *              single day into the statistics file.
+        */
     FILE *fp = fopen("patient.txt","w");
     if(fp == NULL)
     {
